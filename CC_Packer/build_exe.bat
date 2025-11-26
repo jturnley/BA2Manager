241e086ed0143f48d09e609d4f3be8f2b62154cb
Jeremy Turnley <jturnley@duck.com>
2025-11-25 21:04:43 -0800

    Remove CC_Packer directory - now maintained in separate repository

--- CC_Packer/build_exe.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing dependencies...
-pip install -r requirements.txt
-
-echo Building executable...
-pyinstaller --noconfirm --onefile --windowed --name "CCPacker" --clean main.py
-
-echo Build complete! Executable is in the 'dist' folder.
-pause
